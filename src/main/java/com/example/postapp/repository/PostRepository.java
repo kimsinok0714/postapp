8c9130d2fcb2d39c068c8b6ab991606760ec0c00
kimsinok0714 <javaenjoy0714@gmail.com>
2025-10-11 09:51:05 +0900

    Add custom query method to PostRepository

--- src/main/java/com/example/postapp/repository/PostRepository.java
+++ src/main/java/com/example/postapp/repository/PostRepository.java
@@ -1,12 +1,12 @@
 package com.example.postapp.repository;
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.example.postapp.domain.Post;
+
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Integer>, CustomPostRepository {
@@ -25,3 +25,5 @@ public interface PostRepository extends JpaRepository<Post, Integer>, CustomPost
     int getFileCount(@Param(value = "id") long id);
 
 }
+
+
