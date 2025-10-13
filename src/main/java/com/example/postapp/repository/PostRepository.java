e2196f3f5b83f26d820b7a3b9d4342aca2d9c0c7
kimsinok0714 <javaenjoy0714@gmail.com>
2025-10-14 08:45:09 +0900

    Refactor PostRepository queries for clarity

--- src/main/java/com/example/postapp/repository/PostRepository.java
+++ src/main/java/com/example/postapp/repository/PostRepository.java
@@ -18,14 +18,14 @@ public interface PostRepository extends JpaRepository<Post, Integer>, CustomPost
     // @Query("SELECT COUNT(p) FROM Post AS p")
     // int getTotalCount();
 
-    // index : JPQL 표준 함수, List나 Map에 대해 순서, 키를 기준으로 필터링할 수 있도록 지원한다. 
+    // index : JPQL 표준 함수, List인경우 순서를 기준으로 필터링할 수 있도록 지원한다. 
     // index 함수는 @ElementCollection, @OrderColumn이 붙은 List 타입 필드에서만 사용 가능
     // @Query("SELECT p FROM Post p JOIN p.files f ORDER BY index(f)")
     @Query("SELECT p FROM Post p JOIN p.files f WHERE index(f) = 0")   // p 엔티티를 통해서 연관 관계에 있는 files 엔티티를 찹조한다.
     List<Post> findAlls();
 
-    // @Query("SELECT f FROM Post p JOIN p.files f WHERE p.id = :postId AND index(f) = 0")
-    // File findFirstFileByPostId(@Param("postId") Long postId);
+    @Query("SELECT p FROM Post p JOIN p.files f WHERE p.id = :postId AND index(f) = 0")
+    Post findPostByIdWithFirstFile(@Param("postId") Long postId);
 
     @Query("SELECT SIZE(p.files) FROM Post p WHERE p.id = :id")
     int getFileCount(@Param(value = "id") long id);
